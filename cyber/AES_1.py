ef8b429545ddffb7486de85a61ce30e6d1481c04
david50a <108026106+david50a@users.noreply.github.com>
2025-10-29 14:08:31 +0200

    Update AES_1.py

--- cyber/AES_1.py
+++ cyber/AES_1.py
@@ -7,7 +7,7 @@ class AES:
         self.Nr = 10
         self.round_keys = []
         self._key_expansion(key)
-    
+
     # S-box
     _sbox = [
         0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
@@ -23,11 +23,11 @@ class AES:
         0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
         0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
         0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
-        0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0xdc, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d,
-        0x9e, 0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28,
-        0xdf, 0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb
+        0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+        0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+        0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
     ]
-    
+
     # Inverse S-box
     _inv_sbox = [
         0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
@@ -47,21 +47,22 @@ class AES:
         0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
         0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
     ]
-    
+
     # Round constants
-    _Rcon = [0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36]
-    
+    _Rcon = [0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36]
+
     def _key_expansion(self, key):
         """Expand the key into round keys"""
+        # Initialize round keys array with correct size
         self.round_keys = [0] * (4 * self.Nb * (self.Nr + 1))
-        
+
         # Copy the key into the first round key
         for i in range(self.Nk):
-            self.round_keys[i*4] = key[i*4]
-            self.round_keys[i*4+1] = key[i*4+1]
-            self.round_keys[i*4+2] = key[i*4+2]
-            self.round_keys[i*4+3] = key[i*4+3]
-        
+            self.round_keys[i * 4] = key[i * 4]
+            self.round_keys[i * 4 + 1] = key[i * 4 + 1]
+            self.round_keys[i * 4 + 2] = key[i * 4 + 2]
+            self.round_keys[i * 4 + 3] = key[i * 4 + 3]
+
         # All other round keys are found from the previous round keys
         for i in range(self.Nk, self.Nb * (self.Nr + 1)):
             k = (i - 1) * 4
@@ -71,226 +72,237 @@ class AES:
                 self.round_keys[k + 2],
                 self.round_keys[k + 3]
             ]
-            
+
             if i % self.Nk == 0:
                 # Rotate word
                 temp = [temp[1], temp[2], temp[3], temp[0]]
-                
+
                 # SubBytes
                 temp = [self._sbox[b] for b in temp]
-                
+
+                # XOR with Rcon
                 temp[0] ^= self._Rcon[i // self.Nk]
-            
+
             j = i * 4
             k = (i - self.Nk) * 4
             self.round_keys[j] = self.round_keys[k] ^ temp[0]
             self.round_keys[j + 1] = self.round_keys[k + 1] ^ temp[1]
             self.round_keys[j + 2] = self.round_keys[k + 2] ^ temp[2]
             self.round_keys[j + 3] = self.round_keys[k + 3] ^ temp[3]
-    
+
     def _add_round_key(self, state, round_num):
         """Add round key to state"""
         for i in range(16):
-            state[i] ^= self.round_keys[(round_num * self.Nb * 4) + i]
-    
+            state[i] ^= self.round_keys[round_num * 16 + i]
+
     def _sub_bytes(self, state):
         """Apply S-box substitution"""
         for i in range(16):
             state[i] = self._sbox[state[i]]
-    
+
     def _inv_sub_bytes(self, state):
         """Apply inverse S-box substitution"""
         for i in range(16):
             state[i] = self._inv_sbox[state[i]]
-    
+
     def _shift_rows(self, state):
         """Shift rows transformation"""
-        # Rotate first row 1 columns to left
-        temp = state[0 * 4 + 1]
-        state[0 * 4 + 1] = state[1 * 4 + 1]
-        state[1 * 4 + 1] = state[2 * 4 + 1]
-        state[2 * 4 + 1] = state[3 * 4 + 1]
-        state[3 * 4 + 1] = temp
-        
-        # Rotate second row 2 columns to left
-        temp = state[0 * 4 + 2]
-        state[0 * 4 + 2] = state[2 * 4 + 2]
-        state[2 * 4 + 2] = temp
-        
-        temp = state[1 * 4 + 2]
-        state[1 * 4 + 2] = state[3 * 4 + 2]
-        state[3 * 4 + 2] = temp
-        
-        # Rotate third row 3 columns to left
-        temp = state[0 * 4 + 3]
-        state[0 * 4 + 3] = state[3 * 4 + 3]
-        state[3 * 4 + 3] = state[2 * 4 + 3]
-        state[2 * 4 + 3] = state[1 * 4 + 3]
-        state[1 * 4 + 3] = temp
-    
+        # Create a temporary state to avoid overwriting issues
+        temp_state = state[:]
+
+        # Row 0: no shift
+        # Row 1: shift left by 1
+        state[1] = temp_state[5]
+        state[5] = temp_state[9]
+        state[9] = temp_state[13]
+        state[13] = temp_state[1]
+
+        # Row 2: shift left by 2
+        state[2] = temp_state[10]
+        state[6] = temp_state[14]
+        state[10] = temp_state[2]
+        state[14] = temp_state[6]
+
+        # Row 3: shift left by 3
+        state[3] = temp_state[15]
+        state[7] = temp_state[3]
+        state[11] = temp_state[7]
+        state[15] = temp_state[11]
+
     def _inv_shift_rows(self, state):
         """Inverse shift rows transformation"""
-        # Rotate first row 1 columns to right
-        temp = state[3 * 4 + 1]
-        state[3 * 4 + 1] = state[2 * 4 + 1]
-        state[2 * 4 + 1] = state[1 * 4 + 1]
-        state[1 * 4 + 1] = state[0 * 4 + 1]
-        state[0 * 4 + 1] = temp
-        
-        # Rotate second row 2 columns to right
-        temp = state[0 * 4 + 2]
-        state[0 * 4 + 2] = state[2 * 4 + 2]
-        state[2 * 4 + 2] = temp
-        
-        temp = state[1 * 4 + 2]
-        state[1 * 4 + 2] = state[3 * 4 + 2]
-        state[3 * 4 + 2] = temp
-        
-        # Rotate third row 3 columns to right
-        temp = state[0 * 4 + 3]
-        state[0 * 4 + 3] = state[1 * 4 + 3]
-        state[1 * 4 + 3] = state[2 * 4 + 3]
-        state[2 * 4 + 3] = state[3 * 4 + 3]
-        state[3 * 4 + 3] = temp
-    
+        # Create a temporary state to avoid overwriting issues
+        temp_state = state[:]
+
+        # Row 0: no shift
+        # Row 1: shift right by 1
+        state[1] = temp_state[13]
+        state[5] = temp_state[1]
+        state[9] = temp_state[5]
+        state[13] = temp_state[9]
+
+        # Row 2: shift right by 2
+        state[2] = temp_state[10]
+        state[6] = temp_state[14]
+        state[10] = temp_state[2]
+        state[14] = temp_state[6]
+
+        # Row 3: shift right by 3
+        state[3] = temp_state[7]
+        state[7] = temp_state[11]
+        state[11] = temp_state[15]
+        state[15] = temp_state[3]
+
     def _xtime(self, x):
         """Multiplication by 2 in GF(2^8)"""
-        return ((x << 1) ^ (((x >> 7) & 1) * 0x1b)) & 0xFF
-    
+        return ((x << 1) ^ (0x1b if (x & 0x80) else 0x00)) & 0xFF
+
+    def _mix_single_column(self, column):
+        """Mix a single column"""
+        t = column[0] ^ column[1] ^ column[2] ^ column[3]
+        u = column[0]
+        column[0] ^= t ^ self._xtime(column[0] ^ column[1])
+        column[1] ^= t ^ self._xtime(column[1] ^ column[2])
+        column[2] ^= t ^ self._xtime(column[2] ^ column[3])
+        column[3] ^= t ^ self._xtime(column[3] ^ u)
+
     def _mix_columns(self, state):
         """Mix columns transformation"""
         for i in range(4):
-            t = state[i * 4 + 0]
-            Tmp = state[i * 4 + 0] ^ state[i * 4 + 1] ^ state[i * 4 + 2] ^ state[i * 4 + 3]
-            
-            Tm = state[i * 4 + 0] ^ state[i * 4 + 1]
-            Tm = self._xtime(Tm)
-            state[i * 4 + 0] ^= Tm ^ Tmp
-            
-            Tm = state[i * 4 + 1] ^ state[i * 4 + 2]
-            Tm = self._xtime(Tm)
-            state[i * 4 + 1] ^= Tm ^ Tmp
-            
-            Tm = state[i * 4 + 2] ^ state[i * 4 + 3]
-            Tm = self._xtime(Tm)
-            state[i * 4 + 2] ^= Tm ^ Tmp
-            
-            Tm = state[i * 4 + 3] ^ t
-            Tm = self._xtime(Tm)
-            state[i * 4 + 3] ^= Tm ^ Tmp
-    
+            column = [
+                state[i + 0 * 4],
+                state[i + 1 * 4],
+                state[i + 2 * 4],
+                state[i + 3 * 4]
+            ]
+            self._mix_single_column(column)
+            state[i + 0 * 4] = column[0]
+            state[i + 1 * 4] = column[1]
+            state[i + 2 * 4] = column[2]
+            state[i + 3 * 4] = column[3]
+
     def _multiply(self, x, y):
         """Multiplication in GF(2^8)"""
-        return (((y & 1) * x) ^
-                ((y >> 1 & 1) * self._xtime(x)) ^
-                ((y >> 2 & 1) * self._xtime(self._xtime(x))) ^
-                ((y >> 3 & 1) * self._xtime(self._xtime(self._xtime(x)))) ^
-                ((y >> 4 & 1) * self._xtime(self._xtime(self._xtime(self._xtime(x)))))) & 0xFF
-    
+        result = 0
+        for i in range(8):
+            if (y >> i) & 1:
+                result ^= x
+            x = self._xtime(x)
+        return result & 0xFF
+
     def _inv_mix_columns(self, state):
         """Inverse mix columns transformation"""
         for i in range(4):
-            a = state[i * 4 + 0]
-            b = state[i * 4 + 1]
-            c = state[i * 4 + 2]
-            d = state[i * 4 + 3]
-            
-            state[i * 4 + 0] = (self._multiply(a, 0x0e) ^ self._multiply(b, 0x0b) ^ 
-                               self._multiply(c, 0x0d) ^ self._multiply(d, 0x09))
-            state[i * 4 + 1] = (self._multiply(a, 0x09) ^ self._multiply(b, 0x0e) ^ 
-                               self._multiply(c, 0x0b) ^ self._multiply(d, 0x0d))
-            state[i * 4 + 2] = (self._multiply(a, 0x0d) ^ self._multiply(b, 0x09) ^ 
-                               self._multiply(c, 0x0e) ^ self._multiply(d, 0x0b))
-            state[i * 4 + 3] = (self._multiply(a, 0x0b) ^ self._multiply(b, 0x0d) ^ 
-                               self._multiply(c, 0x09) ^ self._multiply(d, 0x0e))
-    
+            column = [
+                state[i + 0 * 4],
+                state[i + 1 * 4],
+                state[i + 2 * 4],
+                state[i + 3 * 4]
+            ]
+
+            u = self._xtime(self._xtime(column[0] ^ column[2]))
+            v = self._xtime(self._xtime(column[1] ^ column[3]))
+            column[0] ^= u
+            column[1] ^= v
+            column[2] ^= u
+            column[3] ^= v
+
+            self._mix_single_column(column)
+
+            state[i + 0 * 4] = column[0]
+            state[i + 1 * 4] = column[1]
+            state[i + 2 * 4] = column[2]
+            state[i + 3 * 4] = column[3]
+
     def _cipher(self, state):
         """AES cipher"""
         self._add_round_key(state, 0)
-        
+
         for round_num in range(1, self.Nr):
             self._sub_bytes(state)
             self._shift_rows(state)
             self._mix_columns(state)
             self._add_round_key(state, round_num)
-        
+
         self._sub_bytes(state)
         self._shift_rows(state)
         self._add_round_key(state, self.Nr)
-    
+
     def _inv_cipher(self, state):
         """AES inverse cipher"""
         self._add_round_key(state, self.Nr)
-        
+
         for round_num in range(self.Nr - 1, 0, -1):
             self._inv_shift_rows(state)
             self._inv_sub_bytes(state)
             self._add_round_key(state, round_num)
             self._inv_mix_columns(state)
-        
+
         self._inv_shift_rows(state)
         self._inv_sub_bytes(state)
         self._add_round_key(state, 0)
-    
+
     def _pkcs7_pad(self, data):
         """Apply PKCS7 padding"""
         if isinstance(data, str):
             data = data.encode('utf-8')
         elif isinstance(data, list):
             data = bytes(data)
-        
+
         pad_len = 16 - (len(data) % 16)
+        if pad_len == 0:
+            pad_len = 16
         padded = bytearray(data)
         for _ in range(pad_len):
             padded.append(pad_len)
         return padded
-    
+
     def _pkcs7_unpad(self, data):
         """Remove PKCS7 padding"""
         if not data:
             return data
-        
+
         pad_len = data[-1]
         if pad_len < 1 or pad_len > 16:
             return data  # Invalid padding
-        
+
         # Verify padding
         for i in range(len(data) - pad_len, len(data)):
             if data[i] != pad_len:
                 return data  # Invalid padding
-        
+
         return data[:-pad_len]
-    
+
     def encrypt(self, plaintext):
         """Encrypt plaintext"""
         padded = self._pkcs7_pad(plaintext)
         encrypted = bytearray()
-        
+
         for i in range(0, len(padded), 16):
-            block = list(padded[i:i+16])
+            block = list(padded[i:i + 16])
             self._cipher(block)
             encrypted.extend(block)
-        
+
         return bytes(encrypted)
-    
+
     def decrypt(self, ciphertext):
         """Decrypt ciphertext"""
         if len(ciphertext) % 16 != 0:
             raise ValueError("Ciphertext length must be multiple of 16")
-        
+
         decrypted = bytearray()
-        
+
         for i in range(0, len(ciphertext), 16):
-            block = list(ciphertext[i:i+16])
+            block = list(ciphertext[i:i + 16])
             self._inv_cipher(block)
             decrypted.extend(block)
-        
+
         return bytes(self._pkcs7_unpad(decrypted))
-    
+
     def to_hex(self, data):
         """Convert bytes to hex string"""
         return data.hex()
-    
+
     def from_hex(self, hex_string):
         """Convert hex string to bytes"""
         return bytes.fromhex(hex_string)
@@ -304,32 +316,32 @@ if __name__ == "__main__":
             0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
             0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
         ])
-        
+
         aes = AES(key)
-        
+
         # Test with text
         text = "Hello, AES encryption! This is a test message."
         print(f"Original text: {text}")
-        
+
         # Encrypt
         encrypted = aes.encrypt(text)
         print(f"Encrypted (hex): {aes.to_hex(encrypted)}")
-        
+
         # Decrypt
         decrypted = aes.decrypt(encrypted)
         decrypted_text = decrypted.decode('utf-8')
         print(f"Decrypted text: {decrypted_text}")
-        
+
         # Test with binary data
         binary_data = bytes([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0xFF, 0xFE, 0xFD])
         print(f"\nOriginal binary (hex): {aes.to_hex(binary_data)}")
-        
+
         encrypted_binary = aes.encrypt(binary_data)
         print(f"Encrypted binary (hex): {aes.to_hex(encrypted_binary)}")
-        
+
         decrypted_binary = aes.decrypt(encrypted_binary)
         print(f"Decrypted binary (hex): {aes.to_hex(decrypted_binary)}")
-        
+
         # Test with data not divisible by 16
         short_data = b"Short!"
         print(f"\nOriginal short data: {short_data}")
@@ -337,7 +349,13 @@ if __name__ == "__main__":
         print(f"Encrypted short (hex): {aes.to_hex(encrypted_short)}")
         decrypted_short = aes.decrypt(encrypted_short)
         print(f"Decrypted short: {decrypted_short}")
-        
+
+        # Verify encryption/decryption works correctly
+        assert text == decrypted_text.decode('utf-8')
+        assert binary_data == decrypted_binary
+        assert short_data == decrypted_short
+        print("\nAll tests passed!")
+
     except ValueError as e:
         print(f"Error: {e}")
     except Exception as e:
